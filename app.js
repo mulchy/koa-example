a63105e0f02ebe197807cacdfd7abfaa48947abb
Ryan Mulcahy <ryan@primeacademy.io>
2016-09-05 23:28:54 -0500

    Understanding middleware.

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var koa = require('koa');
 var app = koa();
 
+// x-response-time
+app.use(function *(next){
+	console.log('inside first handler');
+	var start = new Date;
+	yield next;
+	var ms = new Date - start;
+	this.set('X-Response-Time', ms + 'ms');
+});
+
+// logger
+app.use(function *(next){
+	console.log('inside second handler');
+	var start = new Date;
+	yield next;
+	var ms = new Date - start;
+	console.log('%s %s - %s', this.method, this.url, ms);
+});
+
 app.use(function *(){
-  this.body = 'Hello World';
+	console.log('inside final handler');
+	this.body = 'Hello World';
 });
 
 app.listen(3000);
